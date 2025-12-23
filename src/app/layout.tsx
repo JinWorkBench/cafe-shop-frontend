53db44cc71af3309911e7179d4f161cbe771e361
Sanghwi Jin <brilyent72@gmail.com>
2025-12-23 20:46:01 +0900

    feat: 헤더 Header.tsx 컴포넌트 추가 및 기초 DOM 작성

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import Header from "@/components/common/Header";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -23,8 +24,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         {children}
       </body>
     </html>
---
+++ src/components/common/Header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="w-full border-b border-gray-200">
+      <div className="max-w-5xl mx-auto px-4">
+        <div className="h-16 flex items-center justify-between">
+          <div>Logo</div>
+          <nav>nav</nav>
+        </div>
+      </div>
+    </header>
+  );
+}
