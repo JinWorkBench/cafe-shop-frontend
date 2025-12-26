7f5f639f80369934e27cc62f7d2a7ba20bb62ea2
Sanghwi Jin <brilyent72@gmail.com>
2025-12-26 14:16:19 +0900

    feat: ProductGrid에 사용할 ProductCard 컴포넌트 생성

---
+++ src/components/shop/ProductCard.tsx
@@ -0,0 +1,7 @@
+export default function ProductCard() {
+  return (
+    <div className="bg-gray-100 rounded-lg aspect-square flex items-center justify-center border border-gray-200">
+      <p className="text-gray-500">상품</p>
+    </div>
+  );
+}
